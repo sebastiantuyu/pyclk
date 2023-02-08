b600b2fdd8de1e7a84b8dfe624c845d8c86eaf06
sebastiantuyu <alextpineiro@gmail.com>
2023-02-08 15:10:17 -0600

    feature: python-version agnositc

--- src/commands/install.py
+++ src/commands/install.py
@@ -4,6 +4,7 @@ from ..utils.utils import run_hooks
 from ..utils.logger import completed_process, loading_process
 from spinners import Spinners
 from threading import Thread
+import subprocess
 
 def install():
   root_path = Context.root_path
@@ -16,13 +17,22 @@ def install():
     print("Creating python_modules folder...")
     os.system(f'cd {root_path}\npython3 -m venv python_modules')
 
+  path_to_python_version = os.path.join(
+    root_path,
+    "python_modules",
+    "lib"
+  )
+  python_version = os.listdir(path_to_python_version)[0]
   path_to_env_folder = os.path.join(
     root_path,
     "python_modules",
     "lib",
-    "python3.8",
+    python_version,
     "site-packages"
   )
+
+  print(f"Installing packages in {path_to_env_folder}...")
+
   Context.args['c_process'] = True
   p_log = Thread(target=loading_process, args=["Installing full project..."])
   p_install = Thread(target=os.system, args=[f"pip install --quiet {packages_list} --target {path_to_env_folder} --upgrade"])
